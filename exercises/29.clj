014c8e726750fbb63e0dd7626b4f29b777d400f2
Dario Ghilardi <darioghilardi@webrain.it>
2016-02-01 12:48:26 +0100

    Solve the exercise 29.

---
+++ exercises/29.clj
@@ -0,0 +1,8 @@
+(in-ns 'user)
+
+(= (#(apply str (re-seq #"[A-Z]+" %) "HeLlO, WoRlD!")
+    "HLOWRD"))
+
+(empty? (#(apply str (re-seq #"[A-Z]+" %)) "nothing"))
+
+(= (#(apply str (re-seq #"[A-Z]+" %)) "$#A(*&987Zf") "AZ")
